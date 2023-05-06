4a6cffea0cf2a2595e0a25c15d48e40f1ec7e952
Marto <mhasmat@gmail.com>
2023-05-05 22:07:57 -0300

    Siguiente commit

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "github.com/mhasmat/godesde0/variables"
+import (
+	"fmt"
+
+	"github.com/mhasmat/godesde0/variables"
+)
 
 func main() {
-	variables.MostrarEnteros()
+	// variables.MostrarEnteros()
+	// variables.RestoVariables()
+	estado, texto := variables.ConviertoATexto(1588)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
