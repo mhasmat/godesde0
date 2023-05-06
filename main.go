79963a1b1eca40861914f37a1437d155d94ee854
Marto <mhasmat@gmail.com>
2023-05-05 22:43:27 -0300

    Siguiente commit

--- main.go
+++ main.go
@@ -2,14 +2,36 @@ package main
 
 import (
 	"fmt"
+	// "runtime"
 
-	"github.com/mhasmat/godesde0/variables"
+	"github.com/mhasmat/godesde0/ejercicios"
 )
 
 func main() {
 	// variables.MostrarEnteros()
 	// variables.RestoVariables()
-	estado, texto := variables.ConviertoATexto(1588)
-	fmt.Println(estado)
+	// estado, texto := variables.ConviertoATexto(1588)
+	// fmt.Println(estado)
+	// fmt.Println(texto)
+	/*
+	os := runtime.GOOS
+
+	if os == "linux" || os == "OS X." {
+		fmt.Println("Esto no es Windows, es: ", os)
+	} else {
+		fmt.Println("Esto es Windows: ", os)
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es Linux")
+	case "darwin":
+		fmt.Println("Esto es Darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}*/
+
+	numero, texto := ejercicios.ConversorNumerico("500")
+	fmt.Println(numero)
 	fmt.Println(texto)
 }
